a73edadfc0e70b2ca97db5430e97f66397c29297
silvanacandil-create <silvana.candil@produttivo.com.br>
2026-07-09 00:45:58 -0300

    Conecta Supabase no Streamlit.py

--- database/supabase.py
+++ database/supabase.py
@@ -1,1 +1,10 @@
-# Conexão com o Supabase ficará aqui
+import streamlit as st
+from supabase import create_client
+
+
+@st.cache_resource
+def get_supabase_client():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+
+    return create_client(url, key)
